25038e4ce37aeca040699c13eec6e7b650a6741d
Ankush <ankushsharma9685@gmail.com>
2023-11-02 18:39:44 +0530

    added controller to handel get old messages

--- backend/App/controllers/message.js
+++ backend/App/controllers/message.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { Message } = require("../models")
 
 exports.sendMessage = async (req, res, next) => {
@@ -30,6 +31,52 @@ exports.getMessages = async (req, res, next) => {
         })
     } catch (err) {
         console.log(`${err} in getMessages`);
+        res.status(500).json({
+            message: "failed to get Messages"
+        })
+    }
+}
+
+exports.getMessagesAfterId = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const allMessages = await Message.findAll({
+            where: {
+                id: {
+                    [Op.gt]: id
+                }
+            }
+        });
+
+        res.status(200).json({
+            message: "successfully fetched", data: allMessages
+        })
+    } catch (err) {
+        console.log(`${err} in getMessagesAfterId`);
+        res.status(500).json({
+            message: "failed to get Messages"
+        })
+    }
+}
+
+exports.getMessagesBeforeId = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+
+        const allMessages = await Message.findAll({
+            where: {
+                id: {
+                    [Op.lt]: id
+                }
+            }
+        });
+
+        res.status(200).json({
+            message: "successfully fetched", data: allMessages
+        })
+    } catch (err) {
+        console.log(`${err} in getMessagesBeforeId `);
         res.status(500).json({
             message: "failed to get Messages"
         })
--- backend/App/routes/message.js
+++ backend/App/routes/message.js
@@ -4,5 +4,7 @@ const messageController = require("../controllers/message");
 
 router.post("/send",messageController.sendMessage);
 router.get("/receive",messageController.getMessages);
+router.get("/msg-after-id/:id",messageController.getMessagesAfterId);
+router.get("/msg-before-id/:id",messageController.getMessagesBeforeId);
 
 module.exports = router;
