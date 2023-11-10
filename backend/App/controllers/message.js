d344ecb98644ce789640b60e9f36c80bf9f3573d
Ankush <ankushsharma9685@gmail.com>
2023-11-10 10:21:22 +0530

    changed the message controller to handel individual and group messages

--- backend/App/controllers/message.js
+++ backend/App/controllers/message.js
@@ -1,18 +1,47 @@
 const { Op } = require("sequelize");
 const { Message } = require("../models");
 const sequelize = require("../config/connect");
-const msgLimit = process.env.MSG_LIMIT;
+const { uploadToS3 } = require("../services/s3_service");
+const msgLimit = parseInt(process.env.MSG_LIMIT);
 let totalMsg;
 
 exports.sendMessage = async (req, res, next) => {
     try {
         const userId = req.userId;
-        const { text, groupId } = req.body
-        const message = {
-            userId, text, groupId
+        const data = req.body;
+        const file = req.files?.file;
+        let fileUrl;
+        if (!data) {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+        //check if msg is file then upload else check for text is not empty
+        if (file) {
+            fileUrl = await uploadToS3(file.data, `Messages/${userId}/${file.name}`);
+        } else {
+            if (!data.text || !userId) {
+                return res.status(404).json({
+                    message: "text cannot be empty"
+                })
+            }
+        }
+        //create message to save
+        let message;
+        if (data.groupId) {
+            message = {
+                senderId: userId, text: data.text, groupId: data.groupId, conversationType: "group", fileUrl
+            }
+        } else if (data.receiverId) {
+            message = {
+                senderId: userId, text: data.text, receiverId: data.receiverId, conversationType: "user", fileUrl
+            }
+        } else {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
         }
         // console.log(message);
-
         const result = await Message.create(message);
 
         res.status(200).json({
@@ -29,20 +58,41 @@ exports.sendMessage = async (req, res, next) => {
 exports.getMessages = async (req, res, next) => {
     const t = await sequelize.transaction();
     try {
-        const { groupId } = req.query;
+        const data = req.query;
+        //no query parameters
+        if (!data) {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+        const userId = req.userId;
+        let queryConditions;
+        //check if group or not
+        if (data.groupId) {
+            queryConditions = {
+                groupId: data.groupId,
+                conversationType: "group"
+            }
+        } else if (data.receiverId) {
+            queryConditions = {
+                senderId: userId,
+                receiverId: data.receiverId,
+                conversationType: "user"
+            }
+        } else {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
 
         //count and no of messages
         const [allMessages, totalNoOfMsg] = await Promise.all([Message.findAll({
-            where: {
-                groupId
-            },
+            where: queryConditions,
             order: [['createdAt', 'DESC']],
-            limit: msgLimit
+            limit: msgLimit,
         }, { transaction: t }),
         Message.count({
-            where: {
-                groupId
-            }
+            where: queryConditions
         }, { transaction: t })]);
 
         //check if old msg exist
@@ -65,15 +115,39 @@ exports.getMessages = async (req, res, next) => {
 exports.getMessagesAfterId = async (req, res, next) => {
     try {
         const { id } = req.params;
-        const { groupId } = req.query;
-
-        const allMessages = await Message.findAll({
-            where: {
-                groupId,
+        const data = req.query;
+        //no query parameters
+        if (!data) {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+        let queryConditions;
+        //check if group or not
+        if (data.groupId) {
+            queryConditions = {
+                groupId: data.groupId,
+                conversationType: "group",
                 id: {
                     [Op.gt]: id
                 }
-            },
+            }
+        } else if (data.receiverId) {
+            queryConditions = {
+                receiverId: data.receiverId,
+                conversationType: "user",
+                id: {
+                    [Op.gt]: id
+                }
+            }
+        } else {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+
+        const allMessages = await Message.findAll({
+            where: queryConditions,
             order: [['createdAt', 'ASC']],
             limit: msgLimit
         });
@@ -98,15 +172,39 @@ exports.getMessagesBeforeId = async (req, res, next) => {
     const t = await sequelize.transaction();
     try {
         const { id } = req.params;
-        const { groupId } = req.query;
-
-        const allMessages = await Message.findAll({
-            where: {
-                groupId,
+        const data = req.query;
+        //no query parameters
+        if (!data) {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+        let queryConditions;
+        //check if group or not
+        if (data.groupId) {
+            queryConditions = {
+                groupId: data.groupId,
+                conversationType: "group",
                 id: {
                     [Op.lt]: id
                 }
-            },
+            }
+        } else if (data.receiverId) {
+            queryConditions = {
+                receiverId: data.receiverId,
+                conversationType: "user",
+                id: {
+                    [Op.lt]: id
+                }
+            }
+        } else {
+            return res.status(404).json({
+                message: "empty parameters"
+            })
+        }
+
+        const allMessages = await Message.findAll({
+            where: queryConditions,
             order: [['createdAt', 'ASC']],
             limit: msgLimit
         }, { transaction: t });
