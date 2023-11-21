6a66e1e94985cf4188ff7f26fede1218658ef450
Ankush <ankushsharma9685@gmail.com>
2023-11-21 19:14:25 +0530

    added bulkCreate to query fa

--- backend/App/middlewares/archieveMessages.js
+++ backend/App/middlewares/archieveMessages.js
@@ -7,10 +7,10 @@ const { Message, ArchivedChat } = require('../models');
 
 exports.archieveMessages = async () => {
     // Schedule a task to run at 1am every day
-    cron.schedule('* * * * *', async function () {
+    cron.schedule('0 1 * * *', async function () {
 
         const oneDayAgo = new Date();
-        oneDayAgo.setDate(oneDayAgo.getDate() - 11);
+        oneDayAgo.setDate(oneDayAgo.getDate() - 11);   //  11 days old messages
 
         // Begin a transaction
         const t = await sequelize.transaction();
@@ -27,22 +27,23 @@ exports.archieveMessages = async () => {
                 transaction: t
             })
 
+            // Prepare data for bulk insert
+            const bulkData = oldMessages.map(message => message.toJSON());
 
-            // Create entries in the old messages table
-            // for (let message of oldMessages) {
-            //     await ArchivedChat.create(message.toJSON(), { transaction: t });
-            // }
-            console.log("in", oneDayAgo);
+            // Bulk create entries in the old messages table
+            await ArchivedChat.bulkCreate(bulkData, { transaction: t });
+
             // Delete old messages from the main table
-            // await Message.destroy({ where: {
-            //     where: {
-            //         updatedAt: {
-            //             [Op.lte]: oneDayAgo
-            //         }
-            //     },
-            //     transaction: t
-            // } }, { transaction: t });
+            await Message.destroy({
+                where: {
+                    updatedAt: {
+                        [Op.lte]: oneDayAgo
+                    }
+                },
+                transaction: t
+            });
 
+            // console.log("in", oneDayAgo, bulkData);
             // Commit the transaction
             await t.commit();
         } catch (err) {
